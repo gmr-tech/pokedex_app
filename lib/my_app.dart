67f81641fe0cda73388bd5be9ed404afa16a082b
Renato Marinho Gonçalves <gmr-renato@gmail.com>
2023-07-11 02:03:47 -0300

    refactor: isolate main file to fix linter warnings

---
+++ lib/my_app.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: public_member_api_docs
+
+import 'package:flutter/material.dart';
+
+import 'my_home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
